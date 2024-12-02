b53984932927360f1abbfb02b62f5ab08d022ea2
prgs-slam <13154393957@163.com>
2024-12-03 00:53:02 +0800

    Update event_handler.js

--- static/js/event_handler.js
+++ static/js/event_handler.js
@@ -96,8 +96,57 @@ document.addEventListener('DOMContentLoaded', domReady);
             }
         }
 
-        function replicamonoSceneEvent(idx) {
+        function tumrgbdSceneEvent(idx) {
             let dics = document.querySelectorAll('.b-dics')[1]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 5
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/tum/rgbd/room0/';
+                        break;
+                    case 1:
+                        image.src = 'img/tum/rgbd/room1/';
+                        break;
+                    case 2:
+                        image.src = 'img/tum/rgbd/room2/';
+                        break; 
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'gsicp.jpg';
+                        break;
+                    case 1:
+                        image.src = image.src + 'splatam.jpg';
+                        break;
+                    case 2:
+                        image.src = image.src + 'rtgslam.jpg';
+                        break;
+                    case 3:
+                        image.src = image.src + 'ours.jpg';
+                        break;
+                    case 4:
+                        image.src = image.src + 'gt.jpg';
+                        break;
+
+                }
+            }
+
+            let scene_list = document.getElementById("object-scale-recon").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
+
+
+        function replicamonoSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[2]
             let sections = dics.getElementsByClassName('b-dics__section')
             let imagesLength = 5
             for (let i = 0; i < imagesLength; i++) {
@@ -159,7 +208,7 @@ document.addEventListener('DOMContentLoaded', domReady);
         }
 
         function tummonoSceneEvent(idx) {
-            let dics = document.querySelectorAll('.b-dics')[2]
+            let dics = document.querySelectorAll('.b-dics')[3]
             let sections = dics.getElementsByClassName('b-dics__section')
             let imagesLength = 5
             for (let i = 0; i < imagesLength; i++) {
@@ -191,54 +240,6 @@ document.addEventListener('DOMContentLoaded', domReady);
                     case 4:
                         image.src = image.src + 'gt.jpg';
                         break;
-                }
-            }
-
-            let scene_list = document.getElementById("object-scale-recon").children;
-            for (let i = 0; i < scene_list.length; i++) {
-                if (idx == i) {
-                    scene_list[i].children[0].className = "nav-link active"
-                }
-                else {
-                    scene_list[i].children[0].className = "nav-link"
-                }
-            }
-        }
-
-        function tumrgbdSceneEvent(idx) {
-            let dics = document.querySelectorAll('.b-dics')[3]
-            let sections = dics.getElementsByClassName('b-dics__section')
-            let imagesLength = 5
-            for (let i = 0; i < imagesLength; i++) {
-                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
-                switch (idx) {
-                    case 0:
-                        image.src = 'img/tum/rgbd/room0/';
-                        break;
-                    case 1:
-                        image.src = 'img/tum/rgbd/room1/';
-                        break;
-                    case 2:
-                        image.src = 'img/tum/rgbd/room2/';
-                        break; 
-                }
-                switch (i) {
-                    case 0:
-                        image.src = image.src + 'gsicp.jpg';
-                        break;
-                    case 1:
-                        image.src = image.src + 'splatam.jpg';
-                        break;
-                    case 2:
-                        image.src = image.src + 'rtgslam.jpg';
-                        break;
-                    case 3:
-                        image.src = image.src + 'ours.jpg';
-                        break;
-                    case 4:
-                        image.src = image.src + 'gt.jpg';
-                        break;
-
                 }
             }
 
